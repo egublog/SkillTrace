42be6872c38d9d5fa844e2e92f97d2868a52ddf8
egublog <egukei0729@gmail.com>
2021-08-29 20:55:41 +0900

    create app/Repositories/Eloquent/AreaRepository.php

---
+++ backend/app/Repositories/Eloquent/AreaRepository.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Repositories\Eloquent;
+
+use App\Models\Area;
+use App\Repositories\AreaRepositoryInterface;
+
+final class AreaRepository implements AreaRepositoryInterface
+{
+
+    private $model;
+
+    /**
+     * @param object $model
+     */
+    public function __construct(AreaRepositoryInterface $model)
+    {
+        $this->model = $model;
+    }
+
+    /**
+     * idで検索するためのメソッド。
+     *
+     * @param integer $id
+     * @return Area|null
+     */
+    public function findById(int $id): ?Area
+    {
+        return $this->model
+            ->where('id', $id)
+            ->first();
+    }
+
+    /**
+     * 保存するためのメソッド。
+     *
+     * @param array $savingAssoc 保存する対象の連想配列
+     *                           [
+     *                           ]
+     *
+     * @return Area 保存した内容を返す
+     */
+    public function create(array $savingAssoc)
+    {
+        return $this->model
+            ->create(
+                $savingAssoc
+            );
+    }
+
+    /**
+     * 更新するためのメソッド。
+     *
+     * @param array $savingAssoc 更新する対象の連想配列
+     *                           [
+     *                           ]
+     *
+     * @return null|bool 更新が成功したらtrue, 失敗したらfalseを返す
+     */
+    public function update(array $savingAssoc): ?bool
+    {
+        return $this->model
+            ->update(
+                $savingAssoc
+            );
+    }
+
+    /**
+     * 削除するためのメソッド。
+     *
+     * @param array $deleteAssoc 削除する対象の連想配列
+     *                           [
+     *                           ]
+     *
+     * @return null|bool 削除が成功したらtrue, 失敗したらfalseを返す
+     */
+    public function delete(array $deleteAssoc): ?bool
+    {
+        return $this->model
+            ->delete(
+                $deleteAssoc
+            );
+    }
+}
