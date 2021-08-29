cad887797d4e75cb8deb1f90796548b4ca64c00d
egublog <egukei0729@gmail.com>
2021-08-29 21:01:23 +0900

    create app/Repositories/Eloquent/CategoryRepository.php

---
+++ backend/app/Repositories/Eloquent/CategoryRepository.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Repositories\Eloquent;
+
+use App\Models\Category;
+use App\Repositories\CategoryRepositoryInterface;
+
+final class CategoryRepository implements CategoryRepositoryInterface
+{
+
+    private $model;
+
+    /**
+     * @param object $model
+     */
+    public function __construct(CategoryRepositoryInterface $model)
+    {
+        $this->model = $model;
+    }
+
+    /**
+     * idで検索するためのメソッド。
+     *
+     * @param integer $id
+     * @return Category|null
+     */
+    public function findById(int $id): ?Category
+    {
+        return $this->model
+            ->where('id', $id)
+            ->first();
+    }
+
+    /**
+     * 保存するためのメソッド。
+     *
+     * @param array $savingAssoc 保存する対象の連想配列
+     *                           [
+     *                           ]
+     *
+     * @return Category 保存した内容を返す
+     */
+    public function create(array $savingAssoc)
+    {
+        return $this->model
+            ->create(
+                $savingAssoc
+            );
+    }
+
+    /**
+     * 更新するためのメソッド。
+     *
+     * @param array $savingAssoc 更新する対象の連想配列
+     *                           [
+     *                           ]
+     *
+     * @return null|bool 更新が成功したらtrue, 失敗したらfalseを返す
+     */
+    public function update(array $savingAssoc): ?bool
+    {
+        return $this->model
+            ->update(
+                $savingAssoc
+            );
+    }
+
+    /**
+     * 削除するためのメソッド。
+     *
+     * @param array $deleteAssoc 削除する対象の連想配列
+     *                           [
+     *                           ]
+     *
+     * @return null|bool 削除が成功したらtrue, 失敗したらfalseを返す
+     */
+    public function delete(array $deleteAssoc): ?bool
+    {
+        return $this->model
+            ->delete(
+                $deleteAssoc
+            );
+    }
+}
