34ce8cb0664fcf9ebb18eb80f9f8d1c93fd1685a
egublog <egukei0729@gmail.com>
2021-10-10 03:11:13 +0900

    [ADD] fillable in Trace

--- backend/app/Models/Trace.php
+++ backend/app/Models/Trace.php
@@ -6,7 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Trace extends Model
 {
-    //
+    protected $fillable = [
+        'id',
+        'user_language_id',
+        'img',
+        'category_id',
+        'content',
+        'updated_at',
+        'created_at',
+    ];
+
     public function category()
     {
         return $this->belongsTo('App\Models\Category');
