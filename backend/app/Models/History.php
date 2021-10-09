9571a64d9ff833d16e1cb050a4b49ce5acf7613b
egublog <egukei0729@gmail.com>
2021-10-10 03:10:12 +0900

    [ADD] fillable in History

--- backend/app/Models/History.php
+++ backend/app/Models/History.php
@@ -6,7 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class History extends Model
 {
-    //
+    protected $fillable = [
+        'id',
+        'name'
+        'updated_at',
+        'created_at',
+    ];
+    
     public function users() {
         return $this->hasMany('App\Models\User');
     }
