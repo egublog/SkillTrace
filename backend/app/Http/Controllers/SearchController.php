ed435caa9b0b8de27b452017375f4acda18b2fcf
egublog <egukei0729@gmail.com>
2021-10-30 16:19:30 +0900

    [MODIFY] searchコントローラにてリポジトリパターン

--- backend/app/Http/Controllers/SearchController.php
+++ backend/app/Http/Controllers/SearchController.php
@@ -7,25 +7,35 @@ use App\Models\History;
 use App\Models\Language;
 use App\Queries\SearchUsers;
 use App\Http\Requests\SearchRequest;
+use App\Repositories\AreaRepositoryInterface;
 use App\Services\UserAuthServiceInterface;
 
 class SearchController extends Controller
 {
     protected $userAuthService;
+    protected $areRepository;
+    protected $historyRepository;
+    protected $languageRepository;
 
     public function __construct(
-        UserAuthServiceInterface $userAuthService
+        UserAuthServiceInterface $userAuthService,
+        AreaRepositoryInterface $areRepository,
+        HistoryRepositoryInterface $historyRepository,
+        LanguageRepositoryInterface $languageRepository
     )
     {
         $this->userAuthService = $userAuthService;
+        $this->areRepository   = $areRepository;
+        $this->historyRepository = $historyRepository;
+        $this->languageRepository = $languageRepository;
     }
 
     public function index()
     {
-        $myId  = $this->userAuthService->getLoginUserId();
-        $areas = Area::all();
-        $histories = History::all();
-        $languages = Language::all();
+        $myId      = $this->userAuthService->getLoginUserId();
+        $areas     = $this->areRepository->getAll();
+        $histories = $this->historyRepository->getAll();
+        $languages = $this->languageRepository->getAll();
 
 
         return view('MyService.search', compact('myId', 'areas', 'histories', 'languages'));
@@ -33,11 +43,10 @@ class SearchController extends Controller
 
     public function search(SearchRequest $request)
     {
-        $myId  = $this->userAuthService->getLoginUserId();
-
-        $areas = Area::all();
-        $histories = History::all();
-        $languages = Language::all();
+        $myId      = $this->userAuthService->getLoginUserId();
+        $areas     = $this->areRepository->getAll();
+        $histories = $this->historyRepository->getAll();
+        $languages = $this->languageRepository->getAll();
 
         $name = $request->input('name');
         $age = $request->input('age');
