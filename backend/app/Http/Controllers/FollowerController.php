173550120a376624f60c2cc001030533fc8c303f
egublog <egukei0729@gmail.com>
2021-12-01 21:54:48 +0900

    [MODIFY] controllerの処理をUseCase層に移動

--- backend/app/Http/Controllers/FollowerController.php
+++ backend/app/Http/Controllers/FollowerController.php
@@ -4,32 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Repositories\FollowRepositoryInterface;
 use App\Services\UserAuthServiceInterface;
+use App\UseCase\FollowerIndexCaseInterface;
 
 /**
  * フォロワーに関するコントローラー
  */
 class FollowerController extends Controller
 {
-    protected $userAuthService;
-    protected $followRepository;
+    protected $followerIndexCase;
 
     public function __construct(
-        UserAuthServiceInterface $userAuthService,
-        FollowRepositoryInterface $followRepository
+        FollowerIndexCaseInterface $followerIndexCase
     ) {
-        $this->userAuthService  = $userAuthService;
-        $this->followRepository = $followRepository;
+        $this->followerIndexCase = $followerIndexCase;
     }
 
     // NOTE: UseCase層を作ったが、引数の$userIdをどう処理するか迷うため、一旦放置
     public function index(int $userId)
     {
-        $myId = $this->userAuthService->getLoginUserId();
+        $followers = $this->followerIndexCase->handle($userId);
 
-        // NOTE: 自分をfollowしている人を取得
-        $followers = $this->followRepository->getByUserToId($myId);
-        $followers->load('user_follower');
-
-        return view('MyService.friends-list', compact('myId', 'followers', 'userId'));
+        return $followers;
     }
 }
