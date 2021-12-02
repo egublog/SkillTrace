f4d464aa58e9524a7c5760592144c25212ea91f3
egublog <egukei0729@gmail.com>
2021-12-02 17:12:12 +0900

    [MODIFY] storeメソッドとimg_storeメソッドをUseCaseに移動

--- backend/app/Http/Controllers/ProfileController.php
+++ backend/app/Http/Controllers/ProfileController.php
@@ -19,28 +19,19 @@ use App\UseCase\ProfileIndexCaseInterface;
  */
 class ProfileController extends Controller
 {
-    protected $userAuthService;
-    protected $userRepository;
-    protected $areaRepository;
-    protected $historyRepository;
-    protected $languageRepository;
     protected $profileIndexCase;
+    protected $profileStoreCase;
+    protected $profileImgStoreCase;
 
     public function __construct(
-        UserAuthServiceInterface $userAuthService,
-        UserRepositoryInterface $userRepository,
-        AreaRepositoryInterface $areaRepository,
-        HistoryRepositoryInterface $historyRepository,
-        LanguageRepositoryInterface $languageRepository,
-        ProfileIndexCaseInterface $profileIndexCase
+        ProfileIndexCaseInterface $profileIndexCase,
+        ProfileStoreCaseInterface $profileStoreCase,
+        ProfileImgStoreCaseInterface $profileImgStoreCase
     )
     {
-        $this->userAuthService    = $userAuthService;
-        $this->userRepository     = $userRepository;
-        $this->areaRepository     = $areaRepository;
-        $this->historyRepository  = $historyRepository;
-        $this->languageRepository = $languageRepository;
-        $this->profileIndexCase   = $profileIndexCase;
+        $this->profileIndexCase    = $profileIndexCase;
+        $this->profileStoreCase    = $profileStoreCase;
+        $this->profileImgStoreCase = $profileImgStoreCase;
     }
 
     public function index()
@@ -53,27 +44,16 @@ class ProfileController extends Controller
     public function store(ProfileRequest $request)
     {
         $validated = $request->validated();
-        $myId      = $this->userAuthService->getLoginUserId();
-        $account   = $this->userRepository->findById($myId);
+        $store = $this->profileStoreCase->handle($validated);
 
-        $account->update($validated);
-
-        return redirect()->route('home.home', ['userId' => $myId]);
+        return $store;
     }
 
     public function img_store(ProfileImageRequest $request)
     {
         $validated = $request->validated();
-        $userImg   = $validated['profile_img'];
+        $imgStore  = $this->profileImgStoreCase->handle($validated);
 
-        $myId      = $this->userAuthService->getLoginUserId();
-        $myAccount = $this->userRepository->findById($myId);
-
-        $path           = Storage::disk('s3')->putFile('profile_img', $userImg, 'public');
-        $myAccount->img = Storage::disk('s3')->url($path);
-
-        $myAccount->save();
-
-        return redirect()->route('profiles.index');
+        return $imgStore;
     }
 }
