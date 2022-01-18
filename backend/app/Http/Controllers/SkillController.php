cfce205d54e8c88d9b35f2253e06ca581420faf0
egublog <egukei0729@gmail.com>
2022-01-18 20:58:30 +0900

    [DELETE] 必要ないnamespaceを削除

--- backend/app/Http/Controllers/SkillController.php
+++ backend/app/Http/Controllers/SkillController.php
@@ -3,10 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\SkillRequest;
-use App\Models\Language;
 use App\Models\UserLanguage;
-use App\Models\Trace;
-use App\Models\Ability;
 use App\Repositories\UserLanguageRepositoryInterface;
 use App\Repositories\UserRepositoryInterface;
 use App\Services\UserAuthServiceInterface;
